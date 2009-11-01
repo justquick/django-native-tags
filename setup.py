9213b0e3656132db70b3029e1d54ce580582330a
Justin Quick <justquick@gmail.com>
2009-11-01 11:01:23 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+
+setup(name='native_tags',
+      version='0.1',
+      description='Pythonic Templatetags for Django',
+      author='Justin Quick',
+      author_email='justquick@gmail.com',
+      url='http://github.com/justquick/django-native-tags',
+      packages=['native_tags', 'native_tags.contrib', 'native_tags.templatetags'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
