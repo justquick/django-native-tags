0c5e6c4db4ff268cb613658532cefce380414370
Justin Quick <justquick@gmail.com>
2009-11-12 13:44:31 -0500

    added templates for inclusion testing

--- example_project/settings.py
+++ example_project/settings.py
@@ -73,9 +73,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'example_project.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    'templates',
 )
 
 CACHE_BACKEND = 'file://%s' % os.path.join(os.getcwd(), 'cache')
@@ -85,6 +83,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.humanize',
     'native_tags',
 )
 
