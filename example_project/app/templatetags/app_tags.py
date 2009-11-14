b9d25e0bd6f55a22bf3d4277e9c9c5372d90f410
Justin Quick <justquick@gmail.com>
2009-11-13 21:02:28 -0500

    moved test functions to reflect proper native tag usage

---
+++ example_project/app/templatetags/app_tags.py
@@ -0,0 +1,29 @@
+from native_tags.decorators import function, comparison, filter
+
+def dynamic(*a, **kw):
+    return list(a) + sorted(kw.items())
+dynamic = function(dynamic)    
+
+def no_render(*a, **kw):
+     return list(a) + sorted(kw.items())
+no_render = function(no_render, resolve=False)
+
+def myfilter(value, arg):# a, b, c):
+    return value + arg
+myfilter = filter(myfilter)
+    
+def adder(x, y):
+    return x + y
+adder = function(adder, name='add')    
+    
+def cmp_kwargs(**kw):
+    return len(kw)
+cmp_kwargs = comparison(cmp_kwargs)
+
+def myinc(noun):
+    return 'unittest.html', {'noun': noun}
+myinc = function(myinc, inclusion=True)
+
+def ifsomething():
+    return True
+ifsomething = comparison(ifsomething)
