53eb1153ca28638aa5d13b2fb77e68c20a433867
Justin Quick <justquick@gmail.com>
2009-11-13 22:38:58 -0500

    this smart if is really only semi smart, use the djangosnippet for now

--- native_tags/contrib/smart_if.py
+++ native_tags/contrib/smart_if.py
@@ -14,8 +14,15 @@ OPERATORS = {
     'in': lambda x, y: x in y,
 }
 
-def smart_if(var1, operator, var2):
+def semi_smart_if(var1, operator=None, var2=None):
     '''
+    Idea adapted from this djangosnippet: http://www.djangosnippets.org/snippets/1350/
+    
+    WARNING: This is not a functional replacement to the above solution as it currently lacks
+    the capability of multiple operations like
+    
+        {% if a > b and a < b %}
+    
     A smarter {% if %} tag for django templates.
 
     While retaining current Django functionality, it also handles equality,
@@ -30,7 +37,10 @@ def smart_if(var1, operator, var2):
     All supported operators are: ``or``, ``and``, ``in``, ``=`` (or ``==``),
     ``!=``, ``>``, ``>=``, ``<`` and ``<=``.
     '''
+    if operator is None and var2 is None:
+        # {% if user.is_authenticated %}
+        return var1
     if not operator in OPERATORS:
         raise TemplateSyntaxError('%r is not a valid operator.' % operator)
     return OPERATORS[operator](var1, var2)
-smart_if = comparison(smart_if, name='if')
+semi_smart_if = comparison(semi_smart_if, name='if')
