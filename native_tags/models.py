8b1fc74b13e6c573d4592a25ca23b46241f86bcd
Justin Quick <justquick@gmail.com>
2009-11-13 20:52:48 -0500

    no need for LOAD setting anymore, can tell by builtins

--- native_tags/models.py
+++ native_tags/models.py
@@ -1,5 +1,5 @@
 import settings
 
-if settings.LOAD:
+if 'native_tags.templatetags.native' in settings.BUILTIN_TAGS:
     from django.template import add_to_builtins
     add_to_builtins('native_tags.templatetags.native')
