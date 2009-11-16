97aedcbad494eebcbadd93a5b57ead46f2aadea0
Justin Quick <justquick@gmail.com>
2009-11-16 01:21:23 -0500

    example admin, login demo:demo

--- example_project/urls.py
+++ example_project/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
+   (r'^admin/(.*)', admin.site.root),
    (r'^(?P<path>.*)$', 'django.views.static.serve',{'document_root':settings.MEDIA_ROOT}),
 )
