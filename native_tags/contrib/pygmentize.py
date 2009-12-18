6c40feae67fdf16afa0251901e8e33f3822afdc3
Justin Quick <justquick@gmail.com>
2009-12-18 17:59:01 -0500

    fixed pygments to default to a no-op if not installed

--- native_tags/contrib/pygmentize.py
+++ native_tags/contrib/pygmentize.py
@@ -1,6 +1,9 @@
-from pygments.formatters import HtmlFormatter
-from pygments.lexers import get_lexer_by_name
-from pygments import highlight as highlighter
+try:
+    from pygments.formatters import HtmlFormatter
+    from pygments.lexers import get_lexer_by_name
+    from pygments import highlight as highlighter
+except ImportError:
+    highlighter = None
 
 from native_tags.decorators import function, block
 
@@ -17,6 +20,8 @@ def highlight_style(cssclass='highlight', **kwargs):
 
             {% highlight_style code linenos=true %}
     """
+    if highlighter is None:
+        return ''
     return HtmlFormatter(**kwargs).get_style_defs('.%s' % cssclass)
 highlight_style = function(highlight_style)
 
@@ -32,8 +37,10 @@ def highlight(code, lexer, **kwargs):
 
         Example::
 
-            {% highlight_style 'print "Hello World"' python linenos=true %}
+            {% highlight 'print "Hello World"' python linenos=true %}
     """
+    if highlighter is None:
+        return '<pre>%s</pre>' % code
     return highlighter(code or '', get_lexer_by_name(lexer), HtmlFormatter(**kwargs))
 highlight = function(highlight, is_safe=True)
 
@@ -56,5 +63,7 @@ def highlight_block(context, nodelist, lexer, **kwargs):
                 print '{{ request.path }}'
             {% endhighlight_block %}
     """
+    if highlighter is None:
+        return '<pre>%s</pre>' % str(nodelist.render(context) or '')
     return highlighter(nodelist.render(context) or '', get_lexer_by_name(lexer), HtmlFormatter(**kwargs))
 highlight_block = block(highlight_block, is_safe=True)
